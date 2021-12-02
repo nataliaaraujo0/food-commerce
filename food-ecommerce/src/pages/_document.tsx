b6652934c59aa730820cd630ed41f61a637f4e21
alucard <devjrteste@gmail.com>
2021-12-01 23:38:54 -0300

    chore: criando variaveis globais

--- food-ecommerce/src/pages/_document.tsx
+++ food-ecommerce/src/pages/_document.tsx
@@ -6,7 +6,10 @@ export default class MyDocument extends Document {
 
             <Html>
                 <Head>
-
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Aladin&family=
+                    Inter: wght @600; 700&family=Montserrat: wght @400; 700&family=Roboto:
+                     wght @400; 500; 700&display=swap" rel="stylesheet"></link>
                 </Head>
 
                 <body>
