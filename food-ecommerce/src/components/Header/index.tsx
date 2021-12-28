6ed1bf30c064345e9bf09d60a149fb0639c118fc
alucard <devjrteste@gmail.com>
2021-12-28 01:21:12 -0300

    chore: responsive app

--- food-ecommerce/src/components/Header/index.tsx
+++ food-ecommerce/src/components/Header/index.tsx
@@ -1,26 +1,58 @@
+import { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
-
+import Image from "next/image"
 interface HeaderProps {
     onOpenNewClientModal: () => void;
 }
 
 export function Header({ onOpenNewClientModal }: HeaderProps) {
-    return (
-        <header className={styles.container}>
-            <div className={styles.headerWrapper}>
-                <div className={styles.logoWrapper}>
-                    <strong>Healthy Food</strong>
-                </div>
 
-                <div className={styles.navWrapper}>
-                    <nav id="nav">
-                        <a href="">HEALTHY RECIPES</a>
-                        <a href="#blog">BLOG</a>
-                        <a href="#join">JOIN</a>
-                    </nav>
-                    <button onClick={onOpenNewClientModal} >REGISTER</button>
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu);
+    }
+    useEffect(function onFirstMount() {
+        function onScroll() {
+            console.log('onScroll');
+        }
+        window.addEventListener('scroll', onScroll);
+    }, [])
+
+    return (
+        <>
+            <header className={styles.container}>
+                <div className={styles.headerWrapper}>
+
+                    <div className={styles.logoWrapper}>
+                        <strong>Healthy Food</strong>
+                    </div>
+
+                    {toggleMenu && (
+                        <div className={styles.navWrapper}>
+                            <nav>
+                                <a href="">HEALTHY RECIPES</a>
+                                <a href="#blog">BLOG</a>
+                                <a href="#join">JOIN</a>
+                                <button className={styles.registerButtonMobile} onClick={onOpenNewClientModal}>REGISTER</button>
+                            </nav>
+                        </div>
+                    )}
+                    <button className={styles.registerButton} onClick={onOpenNewClientModal}>REGISTER</button>
+
+                    <button className={styles.bar} onClick={toggleNav}>
+                        <Image
+                            src="/images/bar.png"
+                            alt=""
+                            width="100"
+                            height="100"
+                        />
+                    </button>
+
                 </div>
-            </div>
-        </header>
+            </header>
+        </>
+
     )
 }
--- food-ecommerce/src/pages/index.tsx
+++ food-ecommerce/src/pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         <img className={styles.footer}
           src="./images/bloco_final_image.svg" alt="footer" />
         <CatalogFooter />
-        <Footer/>
+        <Footer />
       </main>
 
     </>
